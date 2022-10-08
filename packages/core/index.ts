87ceccdc2e3b936d18cefef2ef03c96361b353ce
Fabian-Lars <fabianlars@fabianlars.de>
2022-10-08 11:57:01 +0200

    fix: fileAppName on Linux. Extends #293 (#310)

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -389,7 +389,7 @@ export async function buildProject(
             fileAppName = fileAppName
               .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
               .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
-              .replace(/[ _]/g, '-')
+              .replace(/[ _.]/g, '-')
               .toLowerCase()
           }
 
