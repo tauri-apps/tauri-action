e9bc7fbb07c8eb0e2916912cb2ece607f7f671ad
FabianLars <fabianlars@fabianlars.de>
2024-06-26 16:58:22 +0200

    regex 101

--- src/build.ts
+++ src/build.ts
@@ -71,7 +71,7 @@ export async function buildProject(
     .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
     .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
     .replace(/[ _.]/g, '-')
-    .replace(/\(\)\[\]\{\}/g, '')
+    .replace(/[()[\]{}]/g, '')
     .toLowerCase();
 
   const workspacePath = getWorkspaceDir(app.tauriPath) ?? app.tauriPath;
