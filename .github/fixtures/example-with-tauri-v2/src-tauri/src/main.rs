6f894e8eb80b6825be9817432b3ad18ab4feaeec
FabianLars <fabianlars@fabianlars.de>
2024-09-25 19:11:24 +0200

    fix fixture

--- .github/fixtures/example-with-tauri-v2/src-tauri/src/main.rs
+++ .github/fixtures/example-with-tauri-v2/src-tauri/src/main.rs
@@ -1,7 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    test_app_lib::run()
 }
