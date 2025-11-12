3c63ddbd5319f3421dd2113943e2a4b33f049475
Fabian-Lars <github@fabianlars.de>
2025-11-12 19:52:21 +0100

    backport: incorrect filename suffix for nsis setup exe

---
+++ .changes/fix-fallback-filename.md
@@ -0,0 +1,5 @@
+---
+action: patch
+---
+
+Fix default file name pattern to match tauri's file names again.
--- src/utils.ts
+++ src/utils.ts
@@ -98,9 +98,20 @@ export function getAssetName(asset: Artifact, pattern?: string) {
       asset as unknown as Record<string, string>,
     );
   } else {
+    if (
+      asset.mode !== 'debug' &&
+      asset.ext !== '.app.tar.gz' &&
+      asset.ext !== '.app.tar.gz.sig' &&
+      asset.name !== 'binary'
+    ) {
+      // See TODO above, in most cases we keep the file name set by tauri's cli.
+      return basename(asset.path);
+    }
+
     const name = basename(asset.path, asset.ext);
     let arch = '';
     let dbg = '';
+    let platform = '';
 
     if (
       asset.ext === '.app.tar.gz' ||
@@ -113,7 +124,12 @@ export function getAssetName(asset: Artifact, pattern?: string) {
     if (asset.mode === 'debug') {
       dbg = '-debug';
     }
-    return name + '_' + asset.platform + arch + dbg + asset.ext;
+
+    if (asset.name === 'binary') {
+      platform = '_' + asset.platform;
+    }
+
+    return name + platform + arch + dbg + asset.ext;
   }
 }
 
