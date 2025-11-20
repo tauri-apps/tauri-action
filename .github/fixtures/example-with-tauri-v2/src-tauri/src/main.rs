a0edd6c27cacc4fb9e5c49daa1c6fbebc1e42003
FabianLars <github@fabianlars.de>
2025-11-20 13:08:09 +0100

    add lib

--- .github/fixtures/example-with-tauri-v2/src-tauri/src/main.rs
+++ .github/fixtures/example-with-tauri-v2/src-tauri/src/main.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    test_app_lib::run()
 }
